5f7a9a57c661a5e0e74622a35874efec9198874b
Ranvir Singh <gokusleeping@gmail.com>
2020-08-27 19:29:17 +0530

    sad

--- src/views/attendence/graph.jsx
+++ src/views/attendence/graph.jsx
@@ -4,6 +4,7 @@ var Component = React.Component;
 var CanvasJSReact = require('./canvasjs.react');
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+var dps = [{x: 1, y: 10}, {x: 2, y: 10}, {x: 3, y: 10}, {x: 4, y: 10}, {x: 5, y: 10}];
 class Graph extends Component {
 	render() {
 		const options = {
@@ -21,48 +22,10 @@ class Graph extends Component {
 				shared: true
 			},
 			data: [
-			{
-				type: "area",
-				name: "GBP",
-				showInLegend: true,
-				xValueFormatString: "MMM YYYY",
-				yValueFormatString: "₹#,##0.##",
-				dataPoints: [
-					{ x: new Date("2017- 01- 01"), y: 84.927},
-					{ x: new Date("2017- 02- 01"), y: 82.609},
-					{ x: new Date("2017- 03- 01"), y: 81.428},
-					{ x: new Date("2017- 04- 01"), y: 83.259},
-					{ x: new Date("2017- 05- 01"), y: 83.153},
-					{ x: new Date("2017- 06- 01"), y: 84.180},
-					{ x: new Date("2017- 07- 01"), y: 84.840},
-					{ x: new Date("2017- 08- 01"), y: 82.671},
-					{ x: new Date("2017- 09- 01"), y: 87.496},
-					{ x: new Date("2017- 10- 01"), y: 86.007},
-					{ x: new Date("2017- 11- 01"), y: 87.233},
-					{ x: new Date("2017- 12- 01"), y: 86.276}
-				]
-			},
-			{
-				type: "area",
-				name: "USD",
-				showInLegend: true,
-				xValueFormatString: "MMM YYYY",
-				yValueFormatString: "₹#,##0.##",
-				dataPoints: [
-					{ x: new Date("2017- 01- 01"), y: 67.515},
-					{ x: new Date("2017- 02- 01"), y: 66.725},
-					{ x: new Date("2017- 03- 01"), y: 64.86},
-					{ x: new Date("2017- 04- 01"), y: 64.29},
-					{ x: new Date("2017- 05- 01"), y: 64.51},
-					{ x: new Date("2017- 06- 01"), y: 64.62},
-					{ x: new Date("2017- 07- 01"), y: 64.2},
-					{ x: new Date("2017- 08- 01"), y: 63.935},
-					{ x: new Date("2017- 09- 01"), y: 65.31},
-					{ x: new Date("2017- 10- 01"), y: 64.75},
-					{ x: new Date("2017- 11- 01"), y: 64.49},
-					{ x: new Date("2017- 12- 01"), y: 63.84}
-				]
-			}
+				{
+					type: "line",
+					dataPoints : dps
+				}
 			]
 		}
 		return (
@@ -72,7 +35,7 @@ class Graph extends Component {
 				/>
 			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
 			</div>
-		);
+			);
 	}
 }
 //module.exports = Graph;
